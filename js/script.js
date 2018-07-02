1cf1070f11b7757af5ca522955aa08f6912530e0
dabalaev <dabalaev88@gmail.com>
2018-07-02 14:48:02 +0300

    интерактивность формы

--- js/script.js
+++ js/script.js
@@ -1,24 +1,50 @@
-		var feedbackLink = document .querySelector(".feedback");
+        var feedbackLink = document.querySelector(".feedback");
 
-        var formPopup = document .querySelector(".write-to-us");
+        var formPopup = document.querySelector(".write-to-us");
 
-        var feedbackClose = document .querySelector(".feedback-close");
+        var feedbackClose = formPopup.querySelector(".feedback-close");
+
+        var form = formPopup.querySelector("form");
+        var userName = formPopup.querySelector("[name=name]");
+        var email = formPopup.querySelector("[name=e-mail]");
+        var comment = formPopup.querySelector("[name=comment]");
 
         feedbackLink.addEventListener("click", function (evt) {
           evt.preventDefault();
           formPopup.classList.add("modal-show");
+          userName.focus();
         });
 
         feedbackClose.addEventListener("click", function (evt) {
           evt.preventDefault();
           formPopup.classList.remove("modal-show");
+          formPopup.classList.remove("modal-error");
         });
 
-        var mapLink = document .querySelector(".contacts-button-map");
+        form.addEventListener("submit", function (evt) {
+          if (!userName.value || !email.value || !comment.value) {
+            evt.preventDefault();
+            formPopup.classList.add("modal-error");
+            formPopup.offsetWidth = formPopup.offsetWidth;
+            console.log("Заполните все поля");
+          }
+        });
 
-        var mapPopup = document .querySelector(".popup-map");
+        window.addEventListener("keydown", function (evt) {
+          if (evt.keyCode === 27) {
+            evt.preventDefault();
+            if (formPopup.classList.contains("modal-show")) {
+              formPopup.classList.remove("modal-show");
+              formPopup.classList.remove("modal-error");
+            }
+          }
+        });
 
-        var mapClose = document .querySelector(".map-close");
+        var mapLink = document.querySelector(".contacts-button-map");
+
+        var mapPopup = document.querySelector(".popup-map");
+
+        var mapClose = document.querySelector(".map-close");
 
         mapLink.addEventListener("click", function (evt) {
           evt.preventDefault();
