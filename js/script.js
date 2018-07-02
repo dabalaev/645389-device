4f7fc1459c509bd0bfd770af8c33bff66bc369ce
dabalaev <dabalaev88@gmail.com>
2018-07-02 10:00:01 +0300

    Перенес скрипты

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+		var feedbackLink = document .querySelector(".feedback");
 
+        var formPopup = document .querySelector(".write-to-us");
+
+        var feedbackClose = document .querySelector(".feedback-close");
+
+        feedbackLink.addEventListener("click", function (evt) {
+          evt.preventDefault();
+          formPopup.classList.add("modal-show");
+        });
+
+        feedbackClose.addEventListener("click", function (evt) {
+          evt.preventDefault();
+          formPopup.classList.remove("modal-show");
+        });
+
+        var mapLink = document .querySelector(".contacts-button-map");
+
+        var mapPopup = document .querySelector(".popup-map");
+
+        var mapClose = document .querySelector(".map-close");
+
+        mapLink.addEventListener("click", function (evt) {
+          evt.preventDefault();
+          mapPopup.classList.add("modal-show");
+        });
+
+        mapClose.addEventListener("click", function (evt) {
+          evt.preventDefault();
+          mapPopup.classList.remove("modal-show");
+        });
